b5df13ccc0f23febb436013041c8a60c28c6d51c
Miiaroma <miia.romanainen@gmail.com>
2023-01-29 11:48:30 +0200

    Css classes dynamically added.

--- 01-starting-project 2/src/components/CourseGoals/CourseInput/CourseInput.js
+++ 01-starting-project 2/src/components/CourseGoals/CourseInput/CourseInput.js
@@ -5,10 +5,10 @@ import './CourseInput.css';
 
 const CourseInput = props => {
   const [enteredValue, setEnteredValue] = useState('');
- const [isValid, setIsValid] = useState(true);
+  const [isValid, setIsValid] = useState(true);
 
   const goalInputChangeHandler = event => {
-     if (event.target.value.trim().length > 0) {
+    if (event.target.value.trim().length > 0) {
       setIsValid(true);
     }
     setEnteredValue(event.target.value);
@@ -16,7 +16,7 @@ const CourseInput = props => {
 
   const formSubmitHandler = event => {
     event.preventDefault();
-     if (enteredValue.trim().length === 0) {
+    if (enteredValue.trim().length === 0) {
       setIsValid(false);
       return;
     }
@@ -25,17 +25,10 @@ const CourseInput = props => {
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <div className="form-control">
-        <label style={{ color: !isValid ? 'red' : 'black' }}>Course Goal</label>
-        <input
-          style={{
-            borderColor: !isValid ? 'red' : '#ccc',
-            background: !isValid ? 'salmon' : 'transparent'
-          }}
-          type="text"
-          onChange={goalInputChangeHandler}
-        />
-      </div>        
+      <div className={`form-control ${!isValid ? 'invalid' : ''}`}>
+        <label>Course Goal</label>
+        <input type="text" onChange={goalInputChangeHandler} />
+      </div>
       <Button type="submit">Add Goal</Button>
     </form>
   );
